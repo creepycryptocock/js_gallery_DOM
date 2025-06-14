46e75952a5a46c9e476f76acb9116ebf52b02118
Yurii <j.zherebilov15@gmail.com>
2025-06-15 00:46:16 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function changeImage() {
+  const thumb = document.querySelector('#thumbs');
+  const largeImg = document.querySelector('#largeImg');
+
+  thumb.addEventListener('click', (eventius) => {
+    eventius.preventDefault();
+
+    const li = eventius.target.closest('.list-item');
+
+    if (!li) {
+      return;
+    }
+
+    const link = li.querySelector('a');
+
+    largeImg.src = link.href;
+  });
+}
+
+changeImage();
